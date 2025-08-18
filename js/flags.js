1534a6d1a68f552004706771312a6e51fd8af8ca
joshcabana <cabana.collections2025@gmail.com>
2025-08-19 01:21:54 +1000

    chore(flags): add feature flags and env defaults

---
+++ js/flags.js
@@ -0,0 +1,1 @@
+(function(){ window.CABANA_FLAGS = { checkoutEnabled: (window.NEXT_PUBLIC_CHECKOUT_ENABLED || 'false') === 'true', authEnabled: (window.NEXT_PUBLIC_AUTH_ENABLED || 'false') === 'true' }; })();
